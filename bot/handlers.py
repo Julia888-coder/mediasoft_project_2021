b0ce22c472a7e61d0de195d30bb01ef649417c8e
Julia888-coder <BelozerovaU2002@yandex.ru>
2021-06-02 00:28:26 +0400

    Add pictures in tests, reformat code, edit text and markups etc

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,41 +1,38 @@
-import telebot
+import markups
 from settings import token, answers
-from markups import inf_markup, lectures_markup, tests_markup, empty_markup, articles_markup
 from test import Test
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telebot import TeleBot
 
-bot = telebot.TeleBot(token, parse_mode=None)
+bot = TeleBot(token, parse_mode=None)
 
 
 @bot.message_handler(commands=['start'])
 def send_hello(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['start'], reply_markup=empty_markup())
+    bot.send_message(user_id, answers['start'], reply_markup=markups.empty_markup())
 
 
 @bot.message_handler(commands=['inf'])
 def send_info(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['inf'], reply_markup=inf_markup())
+    bot.send_message(user_id, answers['inf'], reply_markup=markups.inf_markup())
 
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['help'], reply_markup=empty_markup())
+    bot.send_message(user_id, answers['help'], reply_markup=markups.empty_markup())
 
-@bot.message_handler(commands = ['url'])
-def url(message):
-    markup = InlineKeyboardMarkup()
-    btn_my_site= InlineKeyboardButton(text='Гугл диск', url='https://drive.google.com/drive/folders/13GHAaf6VAbaPh8EF'
-                                                            '2v3FxDdZHxB2J6wj?usp=sharing')
-    markup.add(btn_my_site)
-    bot.send_message(message.chat.id, "По ссылке можно найти оформленные презентации, документы и задания. ", reply_markup = markup)
+
+@bot.message_handler(commands=['url'])
+def send_url(message):
+    bot.send_message(message.chat.id, answers['url'], reply_markup=markups.url_markup())
+
 
 @bot.message_handler(commands=['tests'])
 def send_lectures(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['tests'], reply_markup=lectures_markup())
+    bot.send_message(user_id, answers['tests'], reply_markup=markups.lectures_markup())
 
 
 @bot.message_handler(regexp='Книги')
@@ -43,41 +40,47 @@ def send_books(message):
     user_id = message.chat.id
     bot.send_message(user_id, answers['Книги'])
 
+
 @bot.message_handler(regexp='Видеолекции')
-def send_videolec(message):
+def send_video_lectures(message):
     user_id = message.chat.id
     bot.send_message(user_id, answers['Видеолекции'])
 
+
 @bot.message_handler(regexp='Ресурсы')
-def send_resourses(message):
+def send_resources(message):
     user_id = message.chat.id
     bot.send_message(user_id, answers['Ресурсы'])
 
+
 @bot.message_handler(regexp='Назад')
-def send_back(message):
+def go_back(message):
     user_id = message.chat.id
-    bot.send_message(user_id, "повторный выбор", reply_markup=inf_markup())
+    bot.send_message(user_id, answers['inf'], reply_markup=markups.inf_markup())
+
 
 @bot.message_handler(regexp='Статьи')
 def send_articles(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['Статьи'], reply_markup=articles_markup())
+    bot.send_message(user_id, answers['Статьи'], reply_markup=markups.articles_markup())
+
 
 @bot.message_handler(regexp='Лекция 1')
-def send_article1(message):
+def send_first_article(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['Лекция 1'], reply_markup=articles_markup())
+    bot.send_message(user_id, answers['Лекция 1'], reply_markup=markups.articles_markup())
+
 
 @bot.message_handler(regexp='Лекция 2')
-def send_article2(message):
+def send_second_article(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['Лекция 2'], reply_markup=articles_markup())
+    bot.send_message(user_id, answers['Лекция 2'], reply_markup=markups.articles_markup())
+
 
 @bot.message_handler(regexp='Лекция 3')
-def send_article3(message):
+def send_third_article(message):
     user_id = message.chat.id
-    bot.send_message(user_id, answers['Лекция 3'], reply_markup=articles_markup())
-
+    bot.send_message(user_id, answers['Лекция 3'], reply_markup=markups.articles_markup())
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -87,7 +90,7 @@ def callback_query(call):
         bot.edit_message_text(text=answers['tests'],
                               chat_id=call.message.chat.id,
                               message_id=call.message.id,
-                              reply_markup=lectures_markup())
+                              reply_markup=markups.lectures_markup())
     else:
         previous_text = call.message.json['text']
         if "выберите тему теста" in previous_text.lower():
@@ -102,4 +105,4 @@ def callback_query(call):
             bot.edit_message_text(text=f"Вы выбрали лекцию «{call_data}». Выберите тему теста:",
                                   chat_id=call.message.chat.id,
                                   message_id=call.message.id,
-                                  reply_markup=tests_markup(call_data[0:call_data.index('.')]))
+                                  reply_markup=markups.tests_markup(call_data[0:call_data.index('.')]))
