7de83921f43ed1c72ae76de8cbd09606ccfe4f60
Julia888-coder <BelozerovaU2002@yandex.ru>
2021-06-02 12:44:47 +0400

    Change token var name

--- bot/settings.py
+++ bot/settings.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # Ключ доступа к сообществу
-ACCESS_KEY = os.getenv('TOKEN')
+token = os.getenv('TOKEN')
 
 with open('./resources/json/lectures.json', 'r', encoding='UTF-8') as read_file:
     lectures = json.load(read_file)
