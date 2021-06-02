69662c9deb1ad0e62a9004e4f63940626ab6b134
Julia888-coder <BelozerovaU2002@yandex.ru>
2021-06-02 12:40:30 +0400

    Change paths of json files

--- bot/settings.py
+++ bot/settings.py
@@ -4,8 +4,8 @@ import os
 # Ключ доступа к сообществу
 ACCESS_KEY = os.getenv('TOKEN')
 
-with open('../resources/json/lectures.json', 'r', encoding='UTF-8') as read_file:
+with open('./resources/json/lectures.json', 'r', encoding='UTF-8') as read_file:
     lectures = json.load(read_file)
 
-with open('../resources/json/answers.json', 'r', encoding='UTF-8') as read_file:
+with open('./resources/json/answers.json', 'r', encoding='UTF-8') as read_file:
     answers = json.load(read_file)
