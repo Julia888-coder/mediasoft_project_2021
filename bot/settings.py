c448b80444e556689a4d5f69e47b38b915090760
Julia888-coder <BelozerovaU2002@yandex.ru>
2021-06-02 12:09:09 +0400

    add text

--- bot/settings.py
+++ bot/settings.py
@@ -1,6 +1,6 @@
 import json
 
-token = '1801800815:AAEjupazugiswS0ne_hM0pZRLvsY5_SUUh0'
+token = '1683925585:AAGHUc0ygvvQnInHHwZxgojcE9UErJdqEsM'
 
 with open('../resources/json/lectures.json', 'r', encoding='UTF-8') as read_file:
     lectures = json.load(read_file)
